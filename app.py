96b7ecd13a9293bc5c8a2f4db416fd65437cb7d9
Seetaram Naik <seetaramnaik44@gmail.com>
2023-04-25 18:27:00 +0530

    changed style

--- app.py
+++ app.py
@@ -16,7 +16,11 @@ file.close()
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
 
@@ -31,8 +35,8 @@ def index():
         y_pro_non_phishing = gbc.predict_proba(x)[0,1]
         # if(y_pred ==1 ):
         pred = "It is {0:.2f} % safe to go ".format(y_pro_phishing*100)
-        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url,output=pred )
-    return render_template("index.html", xx =-1)
+        return render_template('predict.html',xx =round(y_pro_non_phishing,2),url=url,output=pred )
+    return render_template("predict.html", xx =-1)
 
 
 if __name__ == "__main__":
